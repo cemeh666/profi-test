e0a9ade3bd1a3943e74a989d2d33804c3b8587b9
cemeh666 <semen-5555@mail.ru>
2017-12-10 22:19:32 +0300

    Индекс для поля api_token + по умолчанию длина string в DB 191

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -21,6 +21,7 @@ class CreateUsersTable extends Migration
             $table->string('api_token', 60)->unique()->nullable();
             $table->rememberToken();
             $table->timestamps();
+            $table->index(['api_token']);
         });
     }
 
